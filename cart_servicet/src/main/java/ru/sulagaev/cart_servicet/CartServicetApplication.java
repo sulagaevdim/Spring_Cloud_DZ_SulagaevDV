35b020d7c695fa6e2b8e738cd35437773fe674d5
Дмитрий <sulagaevdim@mail.ru>
2024-10-21 17:55:46 +0400

    add microservices

---
+++ cart_servicet/src/main/java/ru/sulagaev/cart_servicet/CartServicetApplication.java
@@ -0,0 +1,13 @@
+package ru.sulagaev.cart_servicet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartServicetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartServicetApplication.class, args);
+    }
+
+}
---
+++ cart_servicet/src/test/java/ru/sulagaev/cart_servicet/CartServicetApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sulagaev.cart_servicet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServicetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
